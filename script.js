44915fbf3a93166a6fd5cf6933ce4855617e9de8
dreamdeveloper002 <bkosemani1@gmail.com>
2020-09-08 23:19:10 +0100

    edabit

--- script.js
+++ script.js
@@ -134,3 +134,16 @@ function isEmpty(s) {
 }
 
 console.log(isEmpty("")); 
+
+
+//Create a function that takes three arguments prob, prize, pay and returns true if prob * prize > pay; otherwise return false
+
+function profitableGamble(prob, prize, pay) {
+	if (prob * prize > pay) {
+		 return true
+	} else {
+		return false
+	}
+}
+
+console.log(profitableGamble(0.2, 50, 9));
