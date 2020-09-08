3f62d159a4a85c32f0cadea7637cb34f61af81ff
dreamdeveloper002 <bkosemani1@gmail.com>
2020-09-08 23:12:58 +0100

    edabit

--- script.js
+++ script.js
@@ -118,3 +118,19 @@ function toArray(obj) {
 
 
 console.log(toArray({ a: 1, b: 2 }));
+
+
+// Create a function that returns true if a string is empty and false otherwise.
+
+
+function isEmpty(s) {
+	
+	if(s === ''){
+		return true
+	}else{
+		return false
+	}
+
+}
+
+console.log(isEmpty("")); 
