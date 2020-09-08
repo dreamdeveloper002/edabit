bfbd8175c76453b68b071feb7d0a9e8fb4912e35
dreamdeveloper002 <bkosemani1@gmail.com>
2020-09-08 23:01:09 +0100

    edabit

--- script.js
+++ script.js
@@ -107,3 +107,14 @@ function getLength(arr) {
 
 
 console.log('result', getLength([1, [2, 3]]));
+
+
+//Write a function that converts an object into an array, where each element represents a key-value pair.
+
+function toArray(obj) {
+	return Object.entries(obj);
+}
+
+
+
+console.log(toArray({ a: 1, b: 2 }));
