5344e3c838d378eee2d13a0d835d8ad1e73fc054
dreamdeveloper002 <bkosemani1@gmail.com>
2020-09-10 21:58:40 +0100

    edabit

--- script.js
+++ script.js
@@ -192,3 +192,14 @@ function dividesEvenly(a, b) {
 }
 
 dividesEvenly(98, 7)
+
+
+//Given an object containing counts of both upvotes and downvotes, return what vote count should be displayed. This is calculated by subtracting the number of downvotes from upvotes;
+
+function getVoteCount(votes) {
+	const { upvotes, downvotes } = votes
+	return  upvotes - downvotes
+}
+
+
+getVoteCount({ upvotes: 13, downvotes: 0 })
