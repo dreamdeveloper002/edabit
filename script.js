2da5bd2336e81d4f374601759b7e28ba8b087842
dreamdeveloper002 <bkosemani1@gmail.com>
2020-09-20 15:56:29 +0100

    edabit

--- script.js
+++ script.js
@@ -273,3 +273,22 @@ function getLastItem(arr) {
 }
 
 getLastItem([1, 2, 3])
+
+
+//Given a total due and an array representing the amount of change in your pocket, 
+//determine whether or not you are able to pay for the item. 
+//Change will always be represented in the following order: quarters, dimes, nickels, pennies.
+
+//To illustrate: changeEnough([25, 20, 5, 0], 4.25) should yield true, 
+//since having 25 quarters, 20 dimes, 5 nickels and 0 pennies gives you 6.25 + 2 + .25 + 0 = 8.50.
+
+
+
+
+function changeEnough(change, amountDue) {
+	let sum = change[0]*0.25 + change[1]*0.1 + change[2]*0.05 + change[3]*0.01;
+	return sum >= amountDue;
+}
+
+
+changeEnough([2, 100, 0, 0], 14.11)
