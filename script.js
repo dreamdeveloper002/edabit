7ad05660d7e4dfcd274961317f68bf6304f0977e
dreamdeveloper002 <bkosemani1@gmail.com>
2020-10-30 22:58:39 +0100

    edabit

--- script.js
+++ script.js
@@ -372,3 +372,15 @@ function removeABC(str) {
 
 
 removeABC("This might be a bit hard");
+
+
+
+//Create a function that takes an array of strings and returns an array with only the strings that have numbers in them. 
+//If there are no strings containing numbers, return an empty array.
+
+
+function numInStr(arr) {
+	return arr.filter(x=>/\d/.test(x));
+}
+
+numInStr(["1a", "a", "2b", "b"])
