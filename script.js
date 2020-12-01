7eb94b885a26a699c8f406307c523b728150633e
dreamdeveloper002 <bkosemani1@gmail.com>
2020-12-01 22:54:38 +0100

    edabit

--- script.js
+++ script.js
@@ -453,3 +453,17 @@ function parseArray(arr) {
 };
 
 parseArray([1, 2, "a", "b"]);
+
+
+//Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+function maxNum(n1,n2) {
+	if (n2>n1){
+	  return n2;
+	}
+  else{
+	return n1;
+  }
+};
+
+maxNum(1000, 400);
