65e051b2cba8af2536340d83e509a8fcd0ee390a
dreamdeveloper002 <bkosemani1@gmail.com>
2020-12-08 22:53:00 +0100

    edabit

--- script.js
+++ script.js
@@ -485,3 +485,13 @@ function operation(a, b, op) {
 }
 
 operation("1",  "2",  "add" );
+
+
+//Given three arguments ⁠— an object obj of the stolen items, the pets name and a value ⁠— return an object with that name and value in it (as key-value pairs).
+
+function addName (obj, name, value) {
+	return {...obj, [name]: value}
+};
+
+
+addName({}, "Brutus", 300)
