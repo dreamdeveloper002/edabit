7f6151fcc122da27940ceb69fcf1f11b97d4c99b
dreamdeveloper002 <bkosemani1@gmail.com>
2020-12-21 23:09:55 +0100

    edabit

--- script.js
+++ script.js
@@ -512,3 +512,21 @@ const arrayOfMultiples = (num, length) => {
 }
 
 arrayOfMultiples(7, 5);
+
+
+// Create a function that takes two numbers as arguments (num, length) and 
+// returns an array of multiples of num up to length.
+
+
+function arrayOfMultiples (num, length) {
+	var result = []
+	for (let i = 1; i <= length; i++) {
+		
+			result.push(num*i)
+			
+	}
+	return result
+}
+
+
+arrayOfMultiples(5, 6);
