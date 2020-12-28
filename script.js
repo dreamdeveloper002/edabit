ec45cbd00d5d5c05b1acb8fa43299fa0ef1e3b56
dreamdeveloper002 <bkosemani1@gmail.com>
2020-12-28 23:25:29 +0100

    edabit

--- script.js
+++ script.js
@@ -557,3 +557,16 @@ function removeABC(str) {
 
 
 removeABC("This might be a bit hard");
+
+
+//Create a function that takes three arguments prob, prize, pay and returns true if prob * prize > pay; otherwise return false
+
+function profitableGamble(prob, prize, pay) {
+	if (prob * prize > pay) {
+		 return true
+	} else {
+		return false
+	}
+}
+
+console.log(profitableGamble(0.2, 50, 9));
